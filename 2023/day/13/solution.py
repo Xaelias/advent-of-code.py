3753122710c55e2a74d35405fdcb97e9672e7e76
Alexis Lesieur <Me@ALesieur.net>
2023-12-26 16:02:26 -0600

    aoc.py] 2023/12/13 - Move to new Base class

--- 2023/day/13/solution.py
+++ 2023/day/13/solution.py
@@ -1,6 +1,5 @@
-from collections.abc import Iterator
-from contextlib import suppress
 from typing import Annotated
+from typing import Any
 from typing import Literal
 from typing import Optional
 from typing import Union
@@ -8,14 +7,12 @@ from typing import Union
 import numpy as np
 import numpy.typing as npt
 from loguru import logger
-from part_zero import Input
-from part_zero import PartZero
-from part_zero import Prompt
+
+from aocl.base import AoCInput
+from aocl.base import Base
 
 ArrayNxM = Annotated[npt.NDArray[Union[bool | str]], Literal["N", "M"]]
 
-with suppress(Exception):
-    logger.level("FAILED", no=41, color="<red>")
 """
     5║
 .#..#║#..#
@@ -58,7 +55,7 @@ def pprint(
         header_height : header_height + len(mirror), margin_width : margin_width + len(mirror[0])
     ] = mirror
 
-    # pre-compute the human readable values
+    # pre-compute the human-readable values
     v_line = [v + 1 for v in v_line]
     h_line = [h + 1 for h in h_line]
 
@@ -70,8 +67,8 @@ def pprint(
         printable_mirror[idx - 1, :margin_width] = list(str(h).ljust(margin_width))
         printable_mirror[idx - 1, -margin_width:] = list(str(h).rjust(margin_width))
 
-    # NB this doesnt account for ╬ where 2 axis would meet since the problem only deals with 1
-    # summetry at a time
+    # NB this doesn't account for ╬ where 2 axis would meet since the problem only deals with 1
+    # symmetry at a time
     for idx, v in enumerate(v_line):
         idx = margin_width + idx + v
         # insert a new column full of ║ where the symmetry exists
@@ -106,90 +103,73 @@ def check_if_mirrored(mirror: ArrayNxM, axis: int, vertical: bool) -> int:
     return left.size - sum([np.count_nonzero(a == b) for a, b in zip(left, right)])
 
 
-class PartOne(PartZero):
+def process_mirror(mirror: ArrayNxM) -> int:
+    for i in range(mirror.shape[0] - 1):
+        value = check_if_mirrored(mirror, i, vertical=False)
+        if value == 0:
+            points = (i + 1) * 100
+            logger.debug(
+                f"Mirror is flipped horizontally between {i + 1} and {i + 2} ({points} points)."
+            )
+            pprint(mirror, h_line=[i])
+            return points
+    else:
+        # same but other direction
+        for j in range(mirror.shape[1] - 1):
+            value = check_if_mirrored(mirror, j, vertical=True)
+            if value == 0:
+                points = j + 1
+                logger.debug(
+                    f"Mirror is flipped vertically between {j + 1} and {j + 2} ({points} point(s))."
+                )
+                pprint(mirror, v_line=[j])
+                return points
+    return 0  # just in case there is no symmetry
+
+
+def process_mirror_part_two(mirror: ArrayNxM) -> int:
+    # we need to ignore the original symmetry axis
+    previous_points = process_mirror(mirror)
+    for i in range(mirror.shape[0] - 1):
+        value = check_if_mirrored(mirror, i, vertical=False)
+        # we need exactly 1 difference
+        if value == 1:
+            points = (i + 1) * 100
+            if points == previous_points:
+                continue
+            logger.debug(
+                f"Mirror is flipped horizontally between {i + 1} and {i + 2} ({points} points)."
+            )
+            pprint(mirror, h_line=[i])
+            return points
+    else:
+        # same but other direction
+        for j in range(mirror.shape[1] - 1):
+            value = check_if_mirrored(mirror, j, vertical=True)
+            if value == 1:
+                points = j + 1
+                if points == previous_points:
+                    continue
+                logger.debug(
+                    f"Mirror is flipped vertically between {j + 1} and {j + 2} ({points} point(s))."
+                )
+                pprint(mirror, v_line=[j])
+                return points
+    return 0
+
+
+class Solution(Base):
     @classmethod
-    def parse(cls, input: Input) -> list[ArrayNxM]:
+    def parse(cls, input_data: AoCInput) -> list[ArrayNxM]:
         mirrors = []
-        for mirror in input.split_on_empty_lines():
+        for mirror in input_data.as_chunks:
             mirrors.append(np.array([list(line) for line in mirror]))
         return mirrors
 
     @classmethod
-    def process_mirror(cls, mirror: ArrayNxM) -> int:
-        for i in range(mirror.shape[0] - 1):
-            value = check_if_mirrored(mirror, i, vertical=False)
-            if value == 0:
-                points = (i + 1) * 100
-                logger.debug(
-                    f"Mirror is flipped horizontally between {i+1} and {i+2} ({points} points)."
-                )
-                pprint(mirror, h_line=[i])
-                return points
-        else:
-            # same but other direction
-            for j in range(mirror.shape[1] - 1):
-                value = check_if_mirrored(mirror, j, vertical=True)
-                if value == 0:
-                    points = j + 1
-                    logger.debug(
-                        f"Mirror is flipped vertically between {j+1} and {j+2} ({points} point(s))."
-                    )
-                    pprint(mirror, v_line=[j])
-                    return points
-        return 0  # just in case there is no symmetry
+    def process_part_one(cls, parsed_input: list[ArrayNxM], **kwargs: Any) -> int:
+        return sum(map(process_mirror, parsed_input))
 
     @classmethod
-    def process(cls, parsed_input: list[ArrayNxM]) -> int:  # type: ignore
-        return sum(map(cls.process_mirror, parsed_input))
-
-
-class PartTwo(PartOne):
-    @classmethod
-    def process_mirror(cls, mirror: ArrayNxM) -> int:
-        # we need to ignore the original symmetry axis
-        previous_points = super().process_mirror(mirror)
-        for i in range(mirror.shape[0] - 1):
-            value = check_if_mirrored(mirror, i, vertical=False)
-            # we need exactly 1 difference
-            if value == 1:
-                points = (i + 1) * 100
-                if points == previous_points:
-                    continue
-                logger.debug(
-                    f"Mirror is flipped horizontally between {i+1} and {i+2} ({points} points)."
-                )
-                pprint(mirror, h_line=[i])
-                return points
-        else:
-            # same but other direction
-            for j in range(mirror.shape[1] - 1):
-                value = check_if_mirrored(mirror, j, vertical=True)
-                if value == 1:
-                    points = j + 1
-                    if points == previous_points:
-                        continue
-                    logger.debug(
-                        f"Mirror is flipped vertically between {j+1} and {j+2} ({points} point(s))."
-                    )
-                    pprint(mirror, v_line=[j])
-                    return points
-        return 0
-
-
-test_input = Input("./test_input")
-real_input = Input("./input")
-
-
-def main() -> Iterator[bool]:
-    yield PartOne.solve(Prompt(test_input, expected=405))
-    yield PartOne.solve(Prompt(real_input, expected=37_975))
-    yield PartTwo.solve(Prompt(test_input, expected=4_00))
-    yield PartTwo.solve(Prompt(real_input, expected=32_497))
-
-
-if __name__ == "__main__":
-    all(main())
-
-
-def test_results() -> None:
-    assert all(main())
+    def process_part_two(cls, parsed_input: Any, **kwargs: Any) -> int:
+        return sum(map(process_mirror_part_two, parsed_input))
