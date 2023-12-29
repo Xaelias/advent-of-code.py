45264d59d677529b6d212f93f6b75ef8bca53a0e
Alexis Lesieur <Me@ALesieur.net>
2023-12-29 15:06:28 -0600

    typing

--- aocl/linalg.py
+++ aocl/linalg.py
@@ -7,10 +7,8 @@ def quadratic_solve(
     *args: tuple[int, int],
 ):
     coeffs, results = zip(*args)
-    coeffs_matrix = [
-        [x**2, x, 1] for x in coeffs
-    ]
+    coeffs_matrix = np.array([[x**2, x, 1] for x in coeffs])
     logger.trace(f"Generated coffs matrix: {coeffs_matrix}")
     a, b, c = np.linalg.solve(coeffs_matrix, results)
     logger.debug(f"Solved {a=} {b=} {c=}")
-    return a * x ** 2 + b * x + c
+    return a * x**2 + b * x + c
