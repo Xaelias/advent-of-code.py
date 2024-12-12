c46a39c350eaf0a5b066f36cc53b631e3b5f0c00
Alexis Lesieur <Me@ALesieur.net>
2024-12-12 09:26:00 -0600

    aoc: y2024/d11: Part one and two using iterative from reddit

--- y2024/d11/solution.py
+++ y2024/d11/solution.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from functools import cache
 from typing import Any
 
@@ -24,6 +25,18 @@ def count_stones(stone: int, blinks: int) -> int:
     return sum(count_stones(stone, blinks - 1) for stone in apply_rules_once(stone))
 
 
+# basically https://www.reddit.com/r/adventofcode/comments/1hbm0al/comment/m1hp3e3/
+def iterative_count(stones: list[int], blinks: int) -> int:
+    counts = {stone: 1 for stone in stones}
+    for _ in range(blinks):
+        new_counts: dict[int, int] = defaultdict(int)
+        for st in counts:
+            for blked in ~(w := len(str(st))) % 2 * divmod(st, 10 ** (w // 2)) or [st * 2024 or 1]:
+                new_counts[blked] += counts[st]
+        counts = new_counts
+    return sum(counts.values())
+
+
 class Solution(Base):
     @classmethod
     def parse(cls, input_data: AoCInput) -> list[int]:
@@ -31,8 +44,12 @@ class Solution(Base):
 
     @classmethod
     def process_part_one(cls, parsed_input: list[int], **kwargs: Any) -> int:
-        return sum([count_stones(stone, 25) for stone in parsed_input])
+        blinks = 25
+        # return sum([count_stones(stone, blinks) for stone in parsed_input])
+        return iterative_count(parsed_input, blinks)
 
     @classmethod
     def process_part_two(cls, parsed_input: list[int], **kwargs: Any) -> int:
-        return sum([count_stones(stone, 75) for stone in parsed_input])
+        blinks = 75
+        # return sum([count_stones(stone, blinks) for stone in parsed_input])
+        return iterative_count(parsed_input, blinks)
