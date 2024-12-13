26497e05ea2939464956d40ec9e62fa1a5d6ba3e
Alexis Lesieur <Me@ALesieur.net>
2024-12-13 10:43:04 -0600

    aoc: 2024/13: Implement programatic solution

--- aocl/linalg.py
+++ aocl/linalg.py
@@ -12,3 +12,37 @@ def quadratic_solve(
     a, b, c = np.linalg.solve(coeffs_matrix, results)
     logger.debug(f"Solved {a=} {b=} {c=}")
     return a * x**2 + b * x + c
+
+
+# for a system that's:
+# 23 * x + 42 y = 78
+# 31 * x + 87 y = -3
+# coeffs = [[23, 42], [31, 87]]
+# results = [78, -3]
+def numpy_system(coeffs: list[list[float | int]], results: list[float | int]) -> list[float]:
+    return np.linalg.solve(coeffs, results)
+
+
+# a1 * x + b1 * y = c1
+# a2 * x + b2 * y = c2
+def fast_dual_system(
+    a1: float,
+    b1: float,
+    c1: float,
+    a2: float,
+    b2: float,
+    c2: float,
+) -> list[float]:
+    try:
+        y = ((a1 * c2) - (a2 * c1)) / ((a1 * b2) - (a2 * b1))
+        x = (c1 - (b1 * y)) / a1
+        return [x, y]
+    except Exception:
+        # just defaulting back because I'm lazy and don't want to tackle all the special cases
+        return numpy_system(
+            [
+                [a1, b1],
+                [a2, b2],
+            ],
+            [c1, c2],
+        )
--- y2024/d13/solution.py
+++ y2024/d13/solution.py
@@ -1,8 +1,8 @@
 from typing import Any
 
-import numpy as np
 import regex
 
+from aocl import linalg
 from aocl.base import AoCInput
 from aocl.base import Base
 
@@ -23,17 +23,20 @@ class Solution(Base):
     @classmethod
     def process_part_one(cls, parsed_input: list[tuple[int, ...]], **kwargs: Any) -> int:
         moves = 0
-        for a1, a2, b1, b2, x, y in parsed_input:
-            coeffs = [
-                [a1, b1],
-                [a2, b2],
-            ]
-            result = [x, y]
+        for a1, a2, b1, b2, c1, c2 in parsed_input:
+            # coeffs = [
+            #     [a1, b1],
+            #     [a2, b2],
+            # ]
+            # result = [c1, c2]
 
-            solved = np.linalg.solve(coeffs, result)
-            a, b = [round(e) for e in solved]
+            # solved = np.linalg.solve(coeffs, result)
+            # a, b = [round(e) for e in solved]
 
-            if a >= 0 and b >= 0 and np.array_equal(np.dot(coeffs, [a, b]), result):
+            a, b = map(int, linalg.fast_dual_system(a1, b1, c1, a2, b2, c2))
+
+            # if a >= 0 and b >= 0 and np.array_equal(np.dot(coeffs, [a, b]), result):
+            if a >= 0 and b >= 0 and a * a1 + b * b1 == c1 and a * a2 + b * b2 == c2:
                 moves += 3 * a + b
 
         return moves
